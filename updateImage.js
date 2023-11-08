5d25c79821b8eea0031b1cc8fb2987f800217114
Hidaya03 <124737946+Hidaya03@users.noreply.github.com>
2023-11-08 23:23:29 +0000

    All work completed

--- updateImage.js
+++ updateImage.js
@@ -10,7 +10,7 @@ buttonOne.addEventListener('click', function() {
     // 1. Select the placeholder and update the src.
     // - The image address should be a picture of a place you want to travel to.
     // - Use Google Search, find a photo you want, right-click, and copy the image address.
-
+placeholder.src = "http://www.islamicteachings.org/forum/uploads/monthly_08_2013/post-4260-0-14870900-1376390236.jpg";
 
 
 
@@ -23,7 +23,7 @@ buttonTwo.addEventListener('click', function() {
     // 2. Select the placeholder and update the src.
     // - The image address should be a picture that represents your mood.
     // - Use Google Search, find a photo you want, right-click, and copy the image address.
-
+placeholder.src = "http://keepcalling.net/wp-content/uploads/2018/08/Screen-Shot-2018-08-29-at-14.03.44.png";
 
 
     
